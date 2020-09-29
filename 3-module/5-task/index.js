0dbe1d25a2c2dd020596e5049c26e7e1699d1c85
Roman <forauto77@yandex.ru>
2020-09-29 21:37:20 +0300

    create function getMinMax

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,33 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
-}
+  let arr = str.split('');
+  let resultArr = [];
+  let minMax = {
+    min: 0,
+    max: 0
+  }
+  
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] == ' ') {
+        arr[i] = ','
+      }
+    };
+  arr = arr.join('');
+  arr = arr.split(',');
+    for (let i = 0; i < arr.length; i++) {
+      if (!isNaN(arr[i])) {
+        resultArr.push(arr[i]);
+      }    
+    };
+  
+  for (let i = 0; i < resultArr.length; i++) {
+    if (+resultArr[i] < minMax.min) {
+      minMax.min = +resultArr[i];
+    };
+    if (+resultArr[i] > minMax.max) {
+      minMax.max = +resultArr[i];
+    };
+  };  
+  return minMax;
+};
